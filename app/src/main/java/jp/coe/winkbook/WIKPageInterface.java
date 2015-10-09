71267dde0d6ea8c9f96d92846568042d7b77e6b9
coe <coe.app@gmail.com>
2015-10-09 15:43:03 +0900

    画面追加

---
+++ app/src/main/java/jp/coe/winkbook/WIKPageInterface.java
@@ -0,0 +1,10 @@
+package jp.coe.winkbook;
+
+/**
+ * Created by user on 2015/10/09.
+ */
+public interface WIKPageInterface {
+    public void nextPage();
+    public void backPage();
+    public void close();
+}
