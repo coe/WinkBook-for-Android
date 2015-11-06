7acd161af01d89ef4c861665fc5f0b6414286c9a
coe <coe.app@gmail.com>
2015-11-06 10:01:40 +0900

    暫定最終

--- app/src/main/java/jp/coe/winkbook/ItemListActivity.java
+++ app/src/main/java/jp/coe/winkbook/ItemListActivity.java
@@ -42,15 +42,6 @@ public class ItemListActivity extends AppCompatActivity
         mTitleList.addLast(getTitle());
         toolbar.setTitle((String)mTitleList.getLast());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
 //        if (findViewById(R.id.item_detail_container) != null) {
 //            // The detail container view will be present only in the
 //            // large-screen layouts (res/values-large and
